75fd3792d805a24e64f0c5cf91a1333cf12b1f18
kolektiv <andrew@xyncro.com>
2015-05-15 23:35:33 +0100

    getting the basics ready to play with

---
+++ workshop/src/Exercise1/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+open System.IO
+
+// Freya
+
+open Freya.Core
+
+let helloWorld =
+    freya {
+        let body = "Hello World"B
+        let! state = Freya.getState
+
+        state.Environment.["owin.ResponseStatusCode"] <- 200
+        state.Environment.["owin.ResponseReasonPhrase"] <- "Awesome"
+        state.Environment.["owin.ResponseBody"] :?> Stream |> fun x -> x.Write (body, 0, body.Length) }
+
+// Katana
+
+open Microsoft.Owin.Hosting
+
+type FreyaApplication () =
+    member __.Configuration () =
+        OwinAppFunc.ofFreya (helloWorld)
+
+// Main
+
+[<EntryPoint>]
+let main _ = 
+    let _ = WebApp.Start<FreyaApplication> ("http://localhost:8080")
+    let _ = Console.ReadLine ()
+    0
