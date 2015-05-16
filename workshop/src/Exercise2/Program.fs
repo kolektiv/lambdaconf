df9182b1d25931df59d497d5c8b2e77811950f94
kolektiv <andrew@xyncro.com>
2015-05-16 09:16:31 +0100

    exercise 2

---
+++ workshop/src/Exercise2/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+// Freya
+
+open Freya.Core
+open Freya.Types.Http
+
+let helloWorld =
+    freya {
+        let  text = "Hello World"B
+
+        do! Freya.setLensPartial Response.statusCode 200
+        do! Freya.setLensPartial Response.reasonPhrase "Awesome"
+        do! Freya.mapLens Response.body (fun x -> x.Write (text, 0, text.Length); x) }
+
+// Katana
+
+open Microsoft.Owin.Hosting
+
+type Exercise () =
+    member __.Configuration () =
+        OwinAppFunc.ofFreya helloWorld
+
+// Entry
+
+[<EntryPoint>]
+let run _ =
+    let _ = WebApp.Start<Exercise> ("http://localhost:8080")
+    let _ = Console.ReadLine ()
+    0
